48d655cfb0ce7f64276a980d42cc752698d87785
Bobchenyx <chen.yixia@northeastern.edu>
2025-12-02 18:09:29 -0800

    fix transformers version dependency issue & modified accelerate launch with multi_gpu num_processes

--- qwen3-hf.sh
+++ qwen3-hf.sh
@@ -1,7 +1,8 @@
-accelerate launch -m lm_eval --model hf_qwen3 \
-    --model_args pretrained=/home/user1/workspace/bobchenyx/Qwen/Qwen3-30B-A3B-Instruct-2507 \
+accelerate launch --multi_gpu --num_processes 4 \
+    -m lm_eval --model hf_qwen3 \
+    --model_args pretrained=../Qwen/Qwen3-30B-A3B-Instruct-2507,parallelize=True \
     --tasks hellaswag \
-    --batch_size 1
+    --batch_size auto
 
 # accelerate launch -m lm_eval --model hf_qwen3 \
 #     --model_args pretrained=/home/user1/workspace/bobchenyx/Qwen/Qwen3-30B-A3B-Instruct-2507,load_in_4bit=True \
